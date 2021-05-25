895153bcc8b561c7838757820d306122e5908be6
rockstar5495 <pratyakshom.05@gmail.com>
2021-05-25 14:05:19 +0000

    sweet: fix bad merge
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,19 +26,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 
-# Kernel
-TARGET_KERNEL_CONFIG := mido_defconfig
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 loop.max_part=16 androidboot.usbconfigfs=true
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-BOARD_KERNEL_PAGESIZE :=  2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
-TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_CLANG_VERSION := proton
->>>>>>> bcdc81b... mido: overlay: Enable MultiUser Overlay
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := sweet,sweetin
 
