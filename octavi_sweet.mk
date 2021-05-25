d8f8778716ce37e5b05bcf5df76db9fdcd1c88f3
rockstar5495 <pratyakshom.05@gmail.com>
2021-05-25 15:40:33 +0000

    sweet: get Octavi-ify
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- octavi_sweet.mk
+++ octavi_sweet.mk
@@ -1,25 +1,24 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The OctaviOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Xtended Stuff
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit common Octavi Stuff
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
-# Xtended Stuff
-XTENDED_BUILD_TYPE := OFFICIAL
-XTENDED_BUILD_MAINTAINER := Pratyaksh.Bharadwaj
+# Octavi Stuff
+OCTAVI_BUILD_TYPE := Official
+OCTAVI_BUILD_MAINTAINER := Pratyaksh.Bharadwaj
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SCREEN_DENSITY := 440
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xtended_sweet
+PRODUCT_NAME := octavi_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
