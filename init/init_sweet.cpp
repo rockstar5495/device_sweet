a4674d1d5a48ebbdce6bba109bc768e8e1c0e77b
Pratyaksh Bharadwaj <54440925+rockstar5495@users.noreply.github.com>
2021-06-09 21:30:42 +0530

    sweet: init: Updated Build FP & Desc. to Redfin June Patch
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -171,8 +171,8 @@ void vendor_load_properties() {
         }
 
     // SafetyNet workaround
-    fingerprint = "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys";
-    description = "redfin-user 11 RQ2A.210505.003 7255357 release-keys";
+    fingerprint = "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys";
+    description = "redfin-user 11 RQ3A.210605.005 7349499 release-keys";
     workaround_snet_properties();
 
     set_ro_build_prop("fingerprint", fingerprint);
